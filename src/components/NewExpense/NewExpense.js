12e86f75351c33288be25f3d9983c65375390b1b
Daniel Cruz Calani <danny@Daniels-MacBook-Pro.local>
2022-07-04 13:35:45 -0400

    :sparkle: Add button rendering conditionally

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
 import './NewExpense.css';
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = enteredExpenseData => {
         const expenseData = {
             ...enteredExpenseData,
@@ -10,12 +13,29 @@ const NewExpense = (props) => {
 
         //* Child to parent communication
         props.onAddExpense(expenseData);
+        setIsEditing(false);
+    };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
     };
 
     return (
         <div className='new-expense'>
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            )}
             {/* Passing data (child to parent) convention is to use prefix 'on' */}
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {isEditing && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
         </div>
     );
 }
